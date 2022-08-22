2032be926fc99bdcb338d53a9dd623926fc7ad65
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-22 15:42:59 +0530

    max/min

--- Aug22Exercises/Program.cs
+++ Aug22Exercises/Program.cs
@@ -14,10 +14,16 @@
             {
                 arr[i] = Convert.ToInt32(Console.ReadLine());
             }
+            int max = arr[0];
+            int min = arr[0];
             for(int i = 0; i < arr.Length; i++)
             {
-                Console.Write("element-{0}:{1}\n",i,arr[i]);
+                if (arr[i] > max)
+                    max = arr[i];
+                if (arr[i] < min)
+                    min = arr[i];
             }
+            Console.WriteLine("Maximum element {0}\nMinimum element {1}", max, min);
             Console.ReadLine();
 
         }
