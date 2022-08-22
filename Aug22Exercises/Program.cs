aafd765524904f09322b49b03adbc3db1e792b72
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-22 19:31:17 +0530

    commit

--- Aug22Exercises/Program.cs
+++ Aug22Exercises/Program.cs
@@ -14,10 +14,28 @@
             {
                 arr[i] = Convert.ToInt32(Console.ReadLine());
             }
-            for(int i = 0; i < arr.Length; i++)
+           
+           int m=0,k=0;
+            int flag = 0;
+            for(int i= 0;i <arr.Length;i++)
             {
-                Console.Write("element-{0}:{1}\n",i,arr[i]);
-            }
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        m = i;
+                        k = arr[i];
+                        flag = 1;
+                        break;
+                    }
+                }
+                    if (flag != 0)
+                    {
+                        break;
+                    }
+                
+             }
+            Console.WriteLine("smallest index {0} and repeating integer {1}",m,k);
             Console.ReadLine();
 
         }
